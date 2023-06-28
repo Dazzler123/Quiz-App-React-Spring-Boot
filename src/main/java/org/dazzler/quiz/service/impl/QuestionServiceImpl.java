2fe0742d56e2714c847a942155ca107d64d11cda
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-28 12:38:28 +0530

    Introduced QuestionServiceImpl

---
+++ src/main/java/org/dazzler/quiz/service/impl/QuestionServiceImpl.java
@@ -0,0 +1,12 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 28-Jun-2023
+ * Time : 12:38 PM
+ * Project Name : Quiz-App-Spring-Boot
+ */
+package org.dazzler.quiz.service.impl;
+
+import org.dazzler.quiz.service.QuestionService;
+
+public class QuestionServiceImpl implements QuestionService {
+}
