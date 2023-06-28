71dd04ac1bf9f53201cdcf614b503fae119f4de8
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-28 13:17:15 +0530

    Content updated

--- src/main/java/org/dazzler/quiz/entity/Question.java
+++ src/main/java/org/dazzler/quiz/entity/Question.java
@@ -6,5 +6,25 @@
  */
 package org.dazzler.quiz.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String id;
+    private String questionText;
+    private String optionOne;
+    private String optionTwo;
+    private String optionThree;
+    private String optionFour;
+    private String answer;
+    private String difficultyLevel;
+    private String technology;
 }
