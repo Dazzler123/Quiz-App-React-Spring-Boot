ffaf9f9ece412a59dac15da55e4baa247e048312
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-28 13:24:10 +0530

    Introduced QuestionDTO class

---
+++ src/main/java/org/dazzler/quiz/dto/QuestionDTO.java
@@ -0,0 +1,28 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 28-Jun-2023
+ * Time : 1:23 PM
+ * Project Name : Quiz-App-Spring-Boot
+ */
+package org.dazzler.quiz.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class QuestionDTO {
+    private String id;
+    private String questionText;
+    private String optionOne;
+    private String optionTwo;
+    private String optionThree;
+    private String optionFour;
+    private String answer;
+    private String difficultyLevel;
+    private String technology;
+}
