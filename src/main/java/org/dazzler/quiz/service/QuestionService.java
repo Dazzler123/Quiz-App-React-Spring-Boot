219567543070d28e6fc03fe41fad87419a7f4fb7
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-28 13:28:29 +0530

    Content updated

--- src/main/java/org/dazzler/quiz/service/QuestionService.java
+++ src/main/java/org/dazzler/quiz/service/QuestionService.java
@@ -1,4 +1,9 @@
 package org.dazzler.quiz.service;
 
+import org.dazzler.quiz.dto.QuestionDTO;
+
+import java.util.ArrayList;
+
 public interface QuestionService {
+    public ArrayList<QuestionDTO> loadAllQuestion();
 }
--- src/main/java/org/dazzler/quiz/service/impl/QuestionServiceImpl.java
+++ src/main/java/org/dazzler/quiz/service/impl/QuestionServiceImpl.java
@@ -6,17 +6,30 @@
  */
 package org.dazzler.quiz.service.impl;
 
+import org.dazzler.quiz.dto.QuestionDTO;
 import org.dazzler.quiz.repo.QuestionRepo;
 import org.dazzler.quiz.service.QuestionService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 
 @Service
 @Transactional
 public class QuestionServiceImpl implements QuestionService {
 
     @Autowired
-    QuestionRepo questionRepo;
+    ModelMapper mapper;
+
+    @Autowired
+    QuestionRepo repo;
+
+    @Override
+    public ArrayList<QuestionDTO> loadAllQuestion() {
+        return mapper.map(repo.findAll(), new TypeToken<ArrayList<QuestionDTO>>() {
+        }.getType());
+    }
 }
