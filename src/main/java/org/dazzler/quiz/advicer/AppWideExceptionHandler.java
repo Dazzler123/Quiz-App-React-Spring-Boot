86b3d6d93af15e7b1660f830e63f344e64e22233
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-29 14:58:03 +0530

    Introduced AppWideExceptionHandler

---
+++ src/main/java/org/dazzler/quiz/advicer/AppWideExceptionHandler.java
@@ -0,0 +1,10 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 29-Jun-2023
+ * Time : 2:57 PM
+ * Project Name : Quiz-App-Spring-Boot
+ */
+package org.dazzler.quiz.advicer;
+
+public class AppWideExceptionHandler {
+}
