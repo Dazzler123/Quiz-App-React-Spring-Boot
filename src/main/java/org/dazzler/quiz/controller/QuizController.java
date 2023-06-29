ee328b16cb12e3f0f3df714a5464b3ce65339521
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-29 20:40:02 +0530

    Introduced QuizController

---
+++ src/main/java/org/dazzler/quiz/controller/QuizController.java
@@ -0,0 +1,15 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 29-Jun-2023
+ * Time : 8:39 PM
+ * Project Name : Quiz-App-Spring-Boot
+ */
+package org.dazzler.quiz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quiz")
+public class QuizController {
+}
