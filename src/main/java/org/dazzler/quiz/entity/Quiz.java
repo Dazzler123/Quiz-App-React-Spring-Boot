0b1a4c4b77380539333b7e0d8b08204f68610cab
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-30 20:33:40 +0530

    Introduced Quiz entity

---
+++ src/main/java/org/dazzler/quiz/entity/Quiz.java
@@ -0,0 +1,11 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 30-Jun-2023
+ * Time : 8:32 PM
+ * Project Name : Quiz-App-Spring-Boot
+ */
+package org.dazzler.quiz.entity;
+
+public class Quiz {
+
+}
