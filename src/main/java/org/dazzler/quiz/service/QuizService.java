54efdb24b7aa7692ea577f99766b37c9133d40a1
Dazzler123 <dasinduhewagamage@gmail.com>
2023-06-30 20:42:12 +0530

    Introduced Service for Quiz

---
+++ src/main/java/org/dazzler/quiz/service/QuizService.java
@@ -0,0 +1,7 @@
+package org.dazzler.quiz.service;
+
+import org.dazzler.quiz.util.ResponseUtil;
+
+public interface QuizService {
+    public ResponseUtil createNewQuiz();
+}
---
+++ src/main/java/org/dazzler/quiz/service/impl/QuizServiceImpl.java
@@ -0,0 +1,17 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 30-Jun-2023
+ * Time : 8:39 PM
+ * Project Name : Quiz-App-Spring-Boot
+ */
+package org.dazzler.quiz.service.impl;
+
+import org.dazzler.quiz.service.QuizService;
+import org.dazzler.quiz.util.ResponseUtil;
+
+public class QuizServiceImpl implements QuizService {
+    @Override
+    public ResponseUtil createNewQuiz() {
+        return null;
+    }
+}
